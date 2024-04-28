79967c8f4919ea628601787be68f3923165d474f
Aynur <ainur2205@gmail.com>
2024-04-28 20:44:20 +0500

    Работа над дизайном

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
 
 import './App.css';
 import './Header.css';
-import axios from 'axios';
+// import { useHistory } from 'react-router-dom';
 import CivilizationParser from './CivilizationParser';
 function App() {
   const imagePath = 'civ5_logo.png';
+  // const history = useHistory();
+  // const handleCivilizationsClick = () => {
+  //     // При нажатии на "Цивилизации" перейти на страницу с компонентом CivilizationParser
+  //     history.push('/civilizations');
+  // };
   return (
     <div className="App">
         <div className="header">
@@ -14,7 +19,22 @@ function App() {
             <input className="input-container" placeholder="Search..."></input>
         </div>
         <div className="content-block">
-            <CivilizationParser />
+            <div className="content-block-blk">
+                <h1 className="content-block-blk-text">Цивилизации</h1>
+            </div>
+            <div className="content-block-blk">
+                <h1 className="content-block-blk-text">Юниты</h1>
+            </div>
+            <div className="content-block-blk">
+                <h1 className="content-block-blk-text">Ресурсы</h1>
+            </div>
+            <div className="content-block-blk">
+                <h1 className="content-block-blk-text">Местности</h1>
+            </div>
+            <div className="content-block-blk">
+                <h1 className="content-block-blk-text">Здания</h1>
+            </div>
+            {/*<CivilizationParser />*/}
         </div>
     </div>
   );
